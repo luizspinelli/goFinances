5374b83bec37b71ffa03a5a48e64f9e76adb4d62
Luiz Spinelli <luiz.luchi@gmail.com>
2022-02-14 07:49:41 -0300

    global styles

---
+++ src/global/styles/styled.d.ts
@@ -0,0 +1,8 @@
+import 'styled-components'
+import theme from './theme'
+
+declare module 'styled-components' {
+  type ThemeTypes = typeof theme
+
+  export interface DefaultTheme extends ThemeTypes { }
+}
